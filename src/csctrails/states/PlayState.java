c2a04ae8f358340c2f32c2b264b756d519e8fd08
Hunter Anderson <ghunteranderson@gmail.com>
2015-10-03 15:52:02 -0500

    Build a generic model loader and added TiledMapTiledLayer functionality to it

--- src/csctrails/states/PlayState.java
+++ src/csctrails/states/PlayState.java
@@ -18,9 +18,10 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 
-import csctrails.elements.Ladder;
+import csctrails.configuration.Configuration;
 import csctrails.elements.Model;
-import csctrails.elements.Platform;
+import csctrails.elements.ModelLoader;
+
 import csctrails.elements.Player;
 import csctrails.elements.Thrower;
 import csctrails.elements.Thrown;
@@ -28,7 +29,7 @@ import csctrails.handlers.GameStateManager;
 import csctrails.handlers.MyInput;
 import csctrails.handlers.PlayContactListener;
 import csctrails.main.Game;
-import csctrails.main.Paths;
+
 
 /**
  * PlayState is the game-state where most of the game play takes
@@ -41,7 +42,13 @@ import csctrails.main.Paths;
  * 
  */
 public class PlayState extends GameState {
-	//private static Random random = new Random(); not sure why I need this - gha
+	
+	private static Configuration cfg;
+	
+	static{
+		cfg = new Configuration();
+		cfg.loadConfiguration("res/config/paths/");
+	}
 	
 	//Box2D Fields
 	private World world;
@@ -76,42 +83,41 @@ public class PlayState extends GameState {
 		b2dCamera = new OrthographicCamera();
 		b2dCamera.setToOrtho(false, Game.V_WIDTH*Game.SCALE/PPM, Game.V_HEIGHT*Game.SCALE/PPM);
 		
-		//Boundary Body
-		new Model(world, "MODEL:BOUNDARY");
 		
-		//Tiled Map Layout
-		map = new TmxMapLoader().load(Paths.TILEDMAP_PLAY_01);
+		
+		//Load TileMap
+		if(cfg.hasProperty("LEVEL_1@PATHS:MAPS")){
+			map = new TmxMapLoader().load(cfg.getProperty("LEVEL_1@PATHS:MAPS"));
+			//Load Tile Map Tile Layers
+			TiledMapTileLayer tmPlatformLeft = (TiledMapTileLayer) map.getLayers().get("platforms_left");
+			TiledMapTileLayer tmPlatformRight = (TiledMapTileLayer) map.getLayers().get("platforms_right");
+			TiledMapTileLayer tmLadders = (TiledMapTileLayer) map.getLayers().get("ladders");
+			//Load Models
+			ModelLoader.tiledMapLoader(tmPlatformLeft, world, "MODEL:PLATFORM_GROUND", "left,ground");
+			ModelLoader.tiledMapLoader(tmPlatformLeft, world, "MODEL:PLATFORM_CEILING", "left,ceiling");
+			ModelLoader.tiledMapLoader(tmPlatformRight, world, "MODEL:PLATFORM_GROUND", "right,ground");
+			ModelLoader.tiledMapLoader(tmPlatformRight, world, "MODEL:PLATFORM_CEILING", "right,ceiling");
+			ModelLoader.tiledMapLoader(tmLadders, world, "MODEL:LADDER", "");
+		} else{
+			map = new TiledMap();
+		}
 		tmr = new OrthogonalTiledMapRenderer(map);
 		tmr.setView(camera);
-		//left platforms
-		TiledMapTileLayer tmtl = (TiledMapTileLayer) map.getLayers().get("platforms_left");
-		String[] leftPlatformTags = {"left"};
-		Platform.loadPlatforms(world, tmtl, leftPlatformTags);
-		//right platforms
-		tmtl = (TiledMapTileLayer) map.getLayers().get("platforms_right");
-		String[] rightPlatformTags = {"right"};
-		Platform.loadPlatforms(world, tmtl, rightPlatformTags);
-		//ladders
-		tmtl = (TiledMapTileLayer) map.getLayers().get("ladders");
-		Ladder.loadLadders(world, tmtl, null);
 		
-		//Object Layout
-		Game.logger.log("GS: Creating Models for " + title);
-
-		Model m = new Model(world, "MODEL:KEY");
-		m.getBody().setTransform(16*24/PPM, 16*29/PPM, 0f);
-		models.add(m);
-		
+		//Boundaries
+		new Model(world, "MODEL:BOUNDARY");
+		//Key
+		models.add(new Model(world, "MODEL:KEY", 16*24, 16*29));
+		//Player
 		player = new Player(world, "MODEL:PLAYER", 16*1, 16*3);
-		models.add(player); // Model must be added to modelList or it will not be rendered - gha 15.9.25
-		
+		models.add(player);
+		//Boss
 		models.add(new Model(world, "MODEL:BOSS", Game.V_WIDTH-16*7, Game.V_HEIGHT-16*5));
-		
+		//Thrower
 		thrower = new Thrower(world, 16, 120);
 		thrower.setProbability(0.08f);
 		thrower.setPosistion(16*19, 16*32);
 		thrower.setActive(true);
-
 		models.add(thrower.throwObject(16*20, 16*7));
 		models.add(thrower.throwObject(16*10, 16*7));
 		models.add(thrower.throwObject(16*20, 16*11));
@@ -126,7 +132,6 @@ public class PlayState extends GameState {
 
 		//Fonts
 		font = new BitmapFont();
-		
 	}
 
 				
