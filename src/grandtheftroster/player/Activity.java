ec9fea9af0b3f906dc9c9dbd4c389066a2f9ccff
Hunter Anderson <ghunteranderson@gmail.com>
2015-10-19 17:13:46 -0500

    Begun Activity superclass for Player

---
+++ src/grandtheftroster/player/Activity.java
@@ -0,0 +1,14 @@
+package grandtheftroster.player;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+
+public interface Activity
+{
+	public abstract void begin();
+	public abstract void end();
+	public abstract void draw(SpriteBatch sb);
+	public abstract void update(float dt);
+	public abstract void dispose();
+	public abstract void handleinput();
+}
